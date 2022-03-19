8bdf410fe635475cc9dcf33a396ffab6ef899063
Clemant <capou.mj@gmail.com>
2022-03-19 07:12:14 -0400

    feature: add react-router & react-router-dom
    - navigation

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,27 @@
 import "../css/App.css"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./About.js"
+import Contact from "./Contact.js"
+import Home from "./Home.js"
+
 function App() {
   return (
-    <div className="App">
-      <p>Bonjour</p>
-    </div>
+    <BrowserRouter>
+     <Link to="home">Accueil</Link>
+     <Link to="about">&Agrave; propos</Link>
+     <Link to="contact">Contact</Link>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+       <Route path="home" element={<Home/>}/>
+       <Route path="about" element={<About/>} />
+       <Route path="contact" element={<Contact/>}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
